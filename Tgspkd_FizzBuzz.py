c36285f31bd3a5d16db2b099ceb71a950a3b21d9
Tom Scott <tomscott@umc-184013.dhcp.missouri.edu>
2022-10-10 10:39:45 -0500

    Cleaner Indentation
    While the initial solution uses less lines, it is less clear than placing the print statements on new and indented lines.

--- Tgspkd_FizzBuzz.py
+++ Tgspkd_FizzBuzz.py
@@ -1,6 +1,10 @@
 
 for x in range(1,100):
-    if x % 3 and x % 5 == 0: print("FizzBuzz")
-    elif x % 3 == 0: print("Fizz")
-    elif x % 5 == 0: print("Buzz")
-    else: print(x)
+    if x % 3 and x % 5 == 0: 
+        print("FizzBuzz")
+    elif x % 3 == 0: 
+        print("Fizz")
+    elif x % 5 == 0: 
+        print("Buzz")
+    else: 
+        print(x)
